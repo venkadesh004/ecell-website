288cb9d5400a9491616d788e57d89faca5df7dbd
venkadesh004 <venkadeshdeveloper@gmail.com>
2023-04-13 04:50:57 +0530

    Updated responsiveness

--- src/Components/QuotesBox/QuotesBox.jsx
+++ src/Components/QuotesBox/QuotesBox.jsx
@@ -2,19 +2,6 @@ import React from 'react'
 import './QuotesBox.css';
 
 function QuotesBox({img, quotes, author, shift}) {
-    if (shift === 1) {
-        return (
-            <div className='QuotesBox'>
-                <div className="left-col-quotes-1">
-                    <p>{quotes}</p>
-                    <h6>- {author}</h6>
-                </div>
-                <div className="right-col-quotes-1">
-                    <img src={img} alt="" />
-                </div>
-            </div>
-        );
-    }
   return (
     <div className='QuotesBox'>
         <div className="left-col-quotes-0">
--- src/Containers/FeedbackContainer/FeedbackContainer.jsx
+++ src/Containers/FeedbackContainer/FeedbackContainer.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import './FeedbackContainer.css'
 
 import Insta from '../../images/instagram-logo.png';
 import Twitter from '../../images/twitter-logo.png';
 
 function FeedbackContainer() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleSubjectChange = (e) => {
+        setSubject(e.target.value);
+    }
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    }
+
+    const sendMessage = () => {
+        if (name === "") {
+            alert("No Name");
+        } else if (email === "") {
+            alert("No Email");
+        } else if (message === "") {
+            alert("No Message");
+        } else {
+            var data = {
+                name: name,
+                email: email,
+                subject: subject,
+                message: message
+            };
+    
+            console.log(data);
+
+            axios.post('https://ecell-website-backend.onrender.com/postData', data).then(response => {
+                console.log(response);
+            });
+        }
+    }
+
   return (
     <div className='FeedbackContainer'>
         <h1>Get in Touch</h1>
@@ -12,15 +58,15 @@ function FeedbackContainer() {
 
         <form className="message">
             <div className="left-message">
-                <input type="text" placeholder='Name' />
-                <input type="email" placeholder='Email' />
-                <input type="text" placeholder='Subject' />
+                <input type="text" placeholder='Name' name='name' value={name} onChange={handleNameChange} />
+                <input type="email" placeholder='Email' name='email' value={email} onChange={handleEmailChange} />
+                <input type="text" placeholder='Subject' name='subject' value={subject} onChange={handleSubjectChange} />
             </div>
             <div className="right-message">
-                <textarea cols="30" rows="10" placeholder='Message'></textarea>
+                <textarea cols="30" rows="10" placeholder='Message' name='message' value={message} onChange={handleMessageChange}></textarea>
             </div>
         </form>
-        <submit className="send">Send Message</submit>
+        <submit className="send" onClick={sendMessage}>Send Message</submit>
 
         <div className="footer">
             <a href="https://www.instagram.com/ecelltce/" target='_blank' rel="noreferrer"><img src={Insta} alt="insta" /></a>
